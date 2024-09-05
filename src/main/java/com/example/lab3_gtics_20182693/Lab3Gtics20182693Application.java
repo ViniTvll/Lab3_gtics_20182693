9fcae48b474b9e5d37a24884ff5c9ea757b43c0a
ViniTvll <vinitavella18@gmail.com>
2024-09-04 20:07:26 -0500

    first commit

---
+++ src/main/java/com/example/lab3_gtics_20182693/Lab3Gtics20182693Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_gtics_20182693;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Gtics20182693Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3Gtics20182693Application.class, args);
+    }
+
+}
